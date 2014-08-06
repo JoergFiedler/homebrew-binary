b49de203ef197338552179075726c6929829bb43
Joerg Fiedler <joerg.fiedler@immobilienscout24.de>
2014-08-06 23:17:48 +0200

    fix terraform dl link

--- terraform.rb
+++ terraform.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Terraform < Formula
   homepage "http://www.terraform.io"
-  url "https://dl.bintray.com/mitchellh/terraform/0.1.1_darwin_amd64.zip"
+  url "http://dl.bintray.com/mitchellh/terraform/terraform_0.1.1_darwin_amd64.zip"
   version '0.1.1'
   sha256 "309aed0ed61586e2682f58b77781f8e9805745a5edd1aebcddf883c9f624a0b9"
 
